6816e06fe586cf8b398a98f5444572212e8aca56
宜志富紹太 <shotagishitomi@gmail.com>
2023-09-04 00:20:49 +0900

    2023/9/3  Todo一覧画面 レイアウト作成

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,8 +10,21 @@
     <link rel="stylesheet" href="{{ asset('/lib/fontawesome/css/all.min.css') }}">
     @yield('style')
 </head>
+
 <body>
     <div class="container-wrapper">
+        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">ToDo App</a>
+            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+            <ul class="navbar-nav px-3">
+                <li class="nav-item text-nowrap">
+                    <a class="nav-link" href="#">ログアウト</a>
+                </li>
+            </ul>
+        </nav>
         <main>
             @yield('content')
         </main>
@@ -22,4 +36,5 @@
     <script src="{{ asset('/lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     @yield('script')
 </body>
+
 </html>
